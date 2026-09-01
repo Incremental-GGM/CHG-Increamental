20cdd3beb8753d8cd197283050a5b8e17236c346
chohangyeol09 <chohangyeol09@gmail.com>
2026-09-01 17:27:04 +0900

    feat : gold up

---
+++ Assets/Scripts/GoldInterface.cs
@@ -0,0 +1,15 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class GoldInterface : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI goldText;
+
+        public void GoldTextChange(int goldValue)
+        {
+            goldText.text = "Gold: " + goldValue;
+        }
+    }
+}
---
+++ Assets/Scripts/GoldSystem.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class GoldSystem : MonoBehaviour
+{
+    public UnityEvent<int> OnGoldChanged;
+    
+    private int _gold;
+    [SerializeField] private int upperGoldValue;
+    
+
+    public void GetGold()
+    {
+        _gold += upperGoldValue;
+        OnGoldChanged.Invoke(_gold);
+    }
+}
